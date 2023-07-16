7c65c9f8d6430e58f6343abaa2744acab9ad8eba
SDev7722.Zinskyi Stanislav <stas.dolon@gmail.com>
2023-07-16 22:33:20 +0300

    add buttons with links pages in index

--- src/route/index.js
+++ src/route/index.js
@@ -41,7 +41,10 @@ router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('index', {})
+  res.render('index', {
+
+    layout: 'index',
+  })
   //                  ↑↑ сюди вводимо JSON дані
 })
 
